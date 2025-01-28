517c02b64e169fd46d07e2d1ebbb123cdba08ba4
Ankit Mohanty <99995980+amohanty03@users.noreply.github.com>
2025-01-28 16:45:15 -0600

    Add show my work button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto">
       <div className="w-full">
+        <FloatingNav navItems={[{ name: "Home", link: "/" }, { name: "About", link: "/about" }]} />
         <Hero />
       </div>
     </main>
